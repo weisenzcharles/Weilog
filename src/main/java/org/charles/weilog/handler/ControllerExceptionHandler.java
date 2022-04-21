9139a25c92b9b3ebac323954de2a241178c68785
Charles Zhang <master@weilog.net>
2022-04-21 21:05:56 +0800

    添加静态页面和模版文件

--- src/main/java/org/charles/weilog/handler/ControllerExceptionHandler.java
+++ src/main/java/org/charles/weilog/handler/ControllerExceptionHandler.java
@@ -1,18 +1,17 @@
 package org.charles.weilog.handler;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.logging.Logger;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
-    private Logger logger = (Logger) LoggerFactory.getLogger(ControllerExceptionHandler.class);
+    private Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
-    public ModelAndView handleException(HttpServletRequest request, HttpServletResponse response, Exception ex) {
+    public ModelAndView handleException(HttpServletRequest request, Exception ex) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("exception", ex);
         modelAndView.setViewName("error/error");
